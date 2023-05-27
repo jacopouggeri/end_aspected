eaf58a001ea542008e17e5a3ba2924362e992752
jacopouggeri <jacopo.uggeri@gmail.com>
2023-05-27 16:51:01 +0100

    Added Curios support.

--- src/main/java/net/jayugg/end_aspected/EndAspected.java
+++ src/main/java/net/jayugg/end_aspected/EndAspected.java
@@ -23,6 +23,7 @@ import net.minecraftforge.event.entity.living.EntityTeleportEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
--- src/main/java/net/jayugg/end_aspected/item/SouvenirItem.java
+++ src/main/java/net/jayugg/end_aspected/item/SouvenirItem.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class SouvenirItem extends Item {
+public class SouvenirItem extends Item{
 
     public SouvenirItem(Properties properties) {
         super(properties);
