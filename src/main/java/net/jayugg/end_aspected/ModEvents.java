c650f4c927ba169e5a55d76d2728cb25a016fa9b
jacopouggeri <jacopo.uggeri@gmail.com>
2023-06-01 14:29:14 +0100

    Added Void Vein and Leaves decay.

--- src/main/java/net/jayugg/end_aspected/ModEvents.java
+++ src/main/java/net/jayugg/end_aspected/ModEvents.java
@@ -3,15 +3,10 @@ package net.jayugg.end_aspected;
 import net.jayugg.end_aspected.entity.ModEntityTypes;
 import net.jayugg.end_aspected.entity.VoidBatEntity;
 import net.jayugg.end_aspected.entity.VoidMiteEntity;
-import net.jayugg.end_aspected.item.ModItems;
 import net.jayugg.end_aspected.item.ModSpawnEggItem;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
